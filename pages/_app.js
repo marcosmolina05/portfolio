327be501047fc858e2d2fd74ec4afb625dd91a4b
marcosmolina05 <marcosmolinaaraoz@gmail.com>
2024-04-03 16:33:23 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -21,15 +21,61 @@ function MyApp({ Component, pageProps }) {
             {" "}
             {/* Coloca el componente Head aquí */}
             <title>Marcos Molina - Portfolio</title>
-            <meta name="description" content="Marcos Molina Araoz. Me especializo como Sales Developer Representative, Appointment Setter, Content Manager y Web Development."/>
-            <meta name="keywords" content="appointment setter, editor de videos, content manager"/>
-            <meta name="author" content="Marcos Molina Araoz"/>
+            <meta
+              name="description"
+              content="Marcos Molina Araoz. Me especializo como Sales Developer Representative, Appointment Setter, Content Manager y Web Development."
+            />
+            <meta
+              name="keywords"
+              content="appointment setter, editor de videos, content manager"
+            />
+            <meta name="author" content="Marcos Molina Araoz" />
             <link rel="icon" href="/favicon/favicon.ico" sizes="16x16" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-            <link rel="icon" href="/favicon/favicon-32x32.png" type="image/png" sizes="32x32" />
-            <link rel="icon" href="/favicon/favicon-96x96.png" type="image/png" sizes="96x96" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>            
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            />
+            <link
+              rel="icon"
+              href="/favicon/favicon-32x32.png"
+              type="image/png"
+              sizes="32x32"
+            />
+            <link
+              rel="icon"
+              href="/favicon/favicon-96x96.png"
+              type="image/png"
+              sizes="96x96"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-touch-icon.png"
+            />
             <link rel="manifest" href="/site.webmanifest" />
+
+
+
+            <meta property="og:url" content="https://marcosmolinaaraoz.com.ar" />
+            <meta property="og:title" content="Marcos Molina" />
+            <meta property="og:locale" content="es-Argentina" />
+            <meta
+              property="og:description"
+              content="Cloud storage: security, accessibility, unlimited storage, collaboration, data protection, synchronization and freeing up space."
+            />
+            <meta
+              property="og:image"
+              content="https://fvvtqpkgirpnlmiemgcn.supabase.co/storage/v1/object/sign/logo/mario_molina.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJsb2dvL21hcmlvX21vbGluYS5wbmciLCJpYXQiOjE2NzAwOTMxOTUsImV4cCI6MTk4NTQ1MzE5NX0.Vvz2TljhLPuPpBiyNBIXgOUs98PsJr921WIKX-Yiess"
+            />
+            <meta property="og:image:type" content="image/png" />
+            <meta property="og:image:width" content="300" />
+            <meta property="og:image:height" content="300" />
+            <meta property="og:image:alt" content="mario_molina logo" />
+
+
+
           </Head>
           <Transition />
           <Component {...pageProps} />
