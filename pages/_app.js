7590a6554e62062e0f0cc9af38c03f766afa5828
marcosmolina05 <marcosmolinaaraoz@gmail.com>
2024-04-03 15:46:53 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,34 @@
-import '../styles/globals.css';
+import Head from "next/head"; // Importa Head correctamente
+import "../styles/globals.css";
 
 //components
-import Layout from '../components/Layout';
-import Transition from '../components/Transition';
+import Layout from "../components/Layout";
+import Transition from "../components/Transition";
 
 // router
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 // framer motion
-import { AnimatePresence, motion } from 'framer-motion'; 
+import { AnimatePresence, motion } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter ();
+  const router = useRouter();
   return (
     <Layout>
-      <AnimatePresence mode='wait'>
-        <motion.div key={router.route} className='h-full'> 
-      <Transition/>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Head>
+            {" "}
+            {/* Coloca el componente Head aquí */}
+            <link rel="icon" href="/favicon/favicon.ico" sizes="16x16" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="icon" href="/favicon/favicon-32x32.png" type="image/png" sizes="32x32" />
+            <link rel="icon" href="/favicon/favicon-96x96.png" type="image/png" sizes="96x96" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>            
+            <link rel="manifest" href="/site.webmanifest" />
+          </Head>
+          <Transition />
+          <Component {...pageProps} />
         </motion.div>
       </AnimatePresence>
     </Layout>
